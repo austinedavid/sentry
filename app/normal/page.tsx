3e6b88a390262d2b39aaf80947887c4ca3bec03f
AustineDavid <davmesoaustine@gmail.com>
2024-11-15 06:18:55 +0100

    this is the latest commit

--- app/normal/page.tsx
+++ app/normal/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import React from "react";
+import * as sentry from "@sentry/nextjs";
 
 const page = () => {
   const handleClick = () => {
+    sentry.captureMessage("this is me capturing before consoling the messages");
     throw new Error("something now working well!!!");
   };
   return (
     <div>
+      s
       <button
         className=" px-3 py-2 rounded-md text-white bg-red-700"
         onClick={handleClick}
